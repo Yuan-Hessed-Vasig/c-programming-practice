c53951ca815ff4dfc1328dc1bef5d1151cabb03d
Yuan-Hessed-Vasig <yuanpercent@gmail.com>
2026-08-16 19:11:47 +0800

    printing a date in legal form using switch statement

---
+++ c-programming-a-modern-approach/5-selection-statements/5.3 - the-switch-statement/date.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main()
+{
+	int month, day, year;
+
+	printf("Enter date (mm/dd/yy): ");
+
+	scanf("%d / %d / %d", &month, &day, &year);
+
+	printf("Dated this ");
+
+	switch (day)
+	{
+	case 1:
+	case 21:
+	case 31:
+		printf("%dst ", day);
+		break;
+	case 2:
+	case 22:
+		printf("%dnd ", day);
+		break;
+	case 3:
+	case 23:
+		printf("%drd ", day);
+		break;
+	default:
+		printf("%dth ", day);
+	}
+
+	printf("day of ");
+
+	switch (month)
+	{
+	case 1:
+		printf("Janary");
+		break;
+	case 2:
+		printf("February");
+		break;
+	case 3:
+		printf("March");
+		break;
+	case 4:
+		printf("April");
+		break;
+	case 5:
+		printf("May");
+		break;
+	case 6:
+		printf("June");
+		break;
+	case 7:
+		printf("July");
+		break;
+	case 8:
+		printf("August");
+		break;
+	case 9:
+		printf("September");
+		break;
+	case 10:
+		printf("October");
+		break;
+	case 11:
+		printf("November");
+		break;
+	case 12:
+		printf("December");
+		break;
+	}
+
+	printf(", 20%d\n", year);
+}
